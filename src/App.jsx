91b6ac3bc49f8f1bc5f767671e4da11074ba17b4
Loki2 <stevloki@ymail.com>
2024-03-31 13:22:55 +0700

    add new navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./layout.scss";
 import Navbar from "./layouts/navbar/Navbar";
+import Home from "./pages/home/Home";
 
 
 const App = () => {
   return (
     <div className="layout">
-      <Navbar />
+      <div className="navigation__bar">
+        <Navbar />
+      </div>
+      <div className="container">
+        <Home />
+      </div>
     </div>
   );
 }
