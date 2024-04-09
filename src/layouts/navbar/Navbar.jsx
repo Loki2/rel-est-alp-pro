0a00a235d99aca1e6f8852d4e988e97587b7e646
Loki2 <stevloki@ymail.com>
2024-04-09 12:27:57 +0700

    update frontend need pages

--- src/layouts/navbar/Navbar.jsx
+++ src/layouts/navbar/Navbar.jsx
@@ -18,15 +18,15 @@ const Navbar = () => {
         </a>
 
         <a href="/">Home</a>
-        <a href="/">Products</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
-        <a href="/">Terms</a>
+        <a href="/products">Products</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/agent">Agents</a>
+        <a href="/term">Terms</a>
       </div>
       <div className="navbar_right">
-        <a href="/" className="btn_login">Sign In</a>
-        <a href="/" className="btn_register">Sign Up</a>
+        <a href="/login" className="btn_login">Sign In</a>
+        <a href="/register" className="btn_register">Sign Up</a>
 
         <div className="menu__bar">
           <CiMenuBurger className="menu__icon" onClick={() => setMenu((prev) => !prev)} />
@@ -34,14 +34,14 @@ const Navbar = () => {
 
         <div className={menu ? "menu__list active" : "menu__list"}>
           <a href="/">Home</a>
-          <a href="/">Products</a>
-          <a href="/">About</a>
-          <a href="/">Contact</a>
-          <a href="/">Agents</a>
-          <a href="/">Terms</a>
+          <a href="/products">Products</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+          <a href="/agent">Agents</a>
+          <a href="/term">Terms</a>
 
-          <a href="/">Register</a>
-          <a href="/">Login</a>
+          <a href="/login">Register</a>
+          <a href="/register">Login</a>
         </div>
       </div>
     </nav>
