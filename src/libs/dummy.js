663d897175507876c793da79501051dda3e47c68
Loki2 <stevloki@ymail.com>
2024-04-11 13:37:18 +0700

    update ui

--- src/libs/dummy.js
+++ src/libs/dummy.js
@@ -103,7 +103,7 @@ export const productData = {
   title: "A Great Beautiful apartment near the beach",
   description: "A Great Beautiful apartment near the beach with full moon view at night and It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
   price: 320,
-  images_urls: [
+  img_urls: [
     "https://images.pexels.com/photos/271618/pexels-photo-271618.jpeg?auto=compress&cs=tinysrgb&w=800",
     "https://images.pexels.com/photos/2724749/pexels-photo-2724749.jpeg?auto=compress&cs=tinysrgb&w=800",
     "https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?auto=compress&cs=tinysrgb&w=800",
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import "./product.scss";
 import Slider from '../../components/slider/Slider';
 import { productData, userData } from '../../libs/dummy';
-import { CiLocationOn } from 'react-icons/ci';
+import { CiBookmarkPlus, CiLocationOn } from 'react-icons/ci';
+import Map from '../../components/map/Map';
+import { IoBedOutline, IoChatboxOutline, IoSchoolOutline } from 'react-icons/io5';
+import { BsActivity, BsCurrencyDollar } from 'react-icons/bs';
+import { MdOutlinePets } from 'react-icons/md';
+import { SlSizeFullscreen } from 'react-icons/sl';
+import { TbBath, TbBusStop } from 'react-icons/tb';
+import { RiRestaurant2Line } from 'react-icons/ri';
+
 
 const Product = () => {
-
 
 
   return (
     <div className='product__view__page'>
       <div className="product__detail">
         <div className="product__detail__wrapper">
-          <Slider images={productData.images_urls} />
+          <Slider images={productData.img_urls} />
 
           <div className="product__information">
             <div className="product__info__top">
@@ -41,7 +48,105 @@ const Product = () => {
 
       <div className="product__features">
         <div className="product__features__wrapper">
-          <h1>Product feature</h1>
+          <p className='feature__title'>General</p>
+
+          <div className="list__vertical">
+            <div className="list__feature">
+              <BsActivity className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>Utility</span>
+                <p>Render is responsible</p>
+              </div>
+            </div>
+
+            <div className="list__feature">
+              <MdOutlinePets className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>Pet Policy</span>
+                <p>Pet Allowed</p>
+              </div>
+            </div>
+
+            <div className="list__feature">
+              <BsCurrencyDollar className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>Property Fees</span>
+                <p>Must have to 3x rent in total household income</p>
+              </div>
+            </div>
+          </div>
+
+          <p className='feature__title'>Sizes</p>
+          <div className="list__horizontal">
+            <div className="list__feature">
+              <SlSizeFullscreen className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>80sqm</span>
+                <p>801sqft</p>
+              </div>
+            </div>
+
+            <div className="list__feature">
+              <IoBedOutline className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>2p</span>
+                <p>Bed Room (s)</p>
+              </div>
+            </div>
+
+            <div className="list__feature">
+              <TbBath className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>1</span>
+                <p>Bath Room (s)</p>
+              </div>
+            </div>
+
+          </div>
+
+          <p className='feature__title'>Nearby Places</p>
+          <div className="list__horizontal">
+            <div className="list__feature">
+              <IoSchoolOutline className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>School</span>
+                <p>200m away</p>
+              </div>
+            </div>
+
+            <div className="list__feature">
+              <TbBusStop className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>Bus Stop</span>
+                <p>100m away</p>
+              </div>
+            </div>
+
+            <div className="list__feature">
+              <RiRestaurant2Line className='list__feature__icon' />
+              <div className="list__feature__text">
+                <span>Restaurant</span>
+                <p>200m away</p>
+              </div>
+            </div>
+
+          </div>
+          <p className='feature__title'>Location</p>
+          <div className="map__container">
+            <Map items={[productData]} />
+          </div>
+
+          <div className="actions">
+            <button>
+              <IoChatboxOutline className='action__icon' />
+              Send the Message
+            </button>
+
+            <button>
+              <CiBookmarkPlus className='action__icon' />
+              Save the Place
+            </button>
+          </div>
         </div>
       </div>
     </div>
